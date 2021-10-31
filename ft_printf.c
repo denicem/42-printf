6ddd34968a06c4a99aedfbdfcfef31ac70afb120
Denice Montemayor <dmontema@2-E-5.42heilbronn.de>
2021-10-31 17:04:55 +0100

    update .gitignore; implement func countArgs to count arguments for the placeholders(%).

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,38 @@
 /*   By: dmontema <dmontema@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/27 02:35:34 by dmontema          #+#    #+#             */
-/*   Updated: 2021/10/27 02:35:36 by dmontema         ###   ########.fr       */
+/*   Updated: 2021/10/31 17:02:47 by dmontema         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+#include "ft_printf.h"
+
+int	ft_countArgs(const char *format)
+{
+	int res;
+
+	res = 0;
+	while (*format)
+	{
+		if (*format == '%')
+			res++;
+		format++;
+	}
+	return (res);
+}
+
+int	ft_printf(const char *format, ...)
+{
+	int args;
+
+	args = ft_countArgs(format);
+
+	return (args);
+}
+
+int main() {
+	printf("%d\n", ft_printf("%d%s%s%c%x"));
+	printf("%%\n");
+	return 0;
+}
