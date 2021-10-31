3f3cd7ee9515c36cc26a8b4bf8665a41251c9261
Denice Montemayor <dmontema@2-E-5.42heilbronn.de>
2021-10-31 22:48:58 +0100

    implement prototype to print strings and characters in combination with arguments (%d, %i, %s, %c, %%); organize test cases

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: dmontema <dmontema@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/27 02:35:34 by dmontema          #+#    #+#             */
-/*   Updated: 2021/10/31 20:04:46 by dmontema         ###   ########.fr       */
+/*   Updated: 2021/10/31 22:45:21 by dmontema         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,37 +38,93 @@ int	ft_printf(const char *format, ...)
 
 	count_args = ft_countArgs(format);
 	if (!count_args)
+	// PROTOTYPE print strings and chars, if there are no arguments
 	{
 		count_args = ft_strlen(format);
 		ft_putstr_fd((char *) format, 1);
 	}
+	// END PROTOTYPE print strings and chars, if there are no arguments
 	else
 	{
 		va_start(args, format);
-		int i = 0;
-		while (i < count_args)
+
+		// PROTOTYPE: printing arguments (i, d, s, c %%) with characters within
+		while (*format)
 		{
-			int val = va_arg(args, int);
-			char *s = ft_itoa(val);
-			ft_putstr_fd(s, 1);
-			i++;
+			if (*format == '%')
+			{
+				format++;
+				if ((*format == 'd') || (*format == 'i'))
+				{
+					int intVal = va_arg(args, int);
+					char *s = ft_itoa(intVal);
+					ft_putstr_fd(s, 1);
+					format++;
+				}
+				else if (*format == 's')
+				{
+					char *str = va_arg(args, char *);
+					ft_putstr_fd(str, 1);
+					format++;
+				}
+				else if (*format == 'c')
+				{
+					char c = va_arg(args, int);
+					ft_putchar_fd(c, 1);
+					format++;
+				}
+				else if (*format == '%')
+				{
+					ft_putchar_fd('%', 1),
+					format++;
+				}
+			}
+			else
+			{
+				ft_putchar_fd(*format, 1);
+				count_args++;
+				format++;
+			}
 		}
+		// END PROTOTYPE: printing arguments (i, d, s, c %%) with characters within
+
+		// PROTOTYPE: printing int values
+		// int i = 0;
+		// while (i < count_args)
+		// {
+		// 	int val = va_arg(args, int);
+		// 	char *s = ft_itoa(val);
+		// 	ft_putstr_fd(s, 1);
+		// 	i++;
+		// }
+		// END PROTOTYPE: printing int values
+
 	}
 
 	return (count_args);
 }
 
 int main() {
-	// printf("%d\n", ft_printf("%c%s%p%d%i%u%x%X%%"));
+	// test for countArgs
+	// printf("%d\n", ft_printf("%c%s%p%d%i%u%x%X%%")); 
 	// printf("%d\n", ft_printf("Hello, my name is %s and I am %d years old. :)\n"));
+
+	// test for printing strings and charcters without any arguments and result variable
 	// int out = ft_printf("Hello, World!\n");
 	// int out1 = ft_printf("H");
 	// int out = printf("Hello, World!\n");
 	// int out1 = printf("%d", 234);
 	// printf("\n%d %d\n", out, out1);
+	// ft_printf("HAPPY HALLOWEEN!!\n");
 
-	int outInt =  ft_printf("%d%d", 42, 24);
-	printf("\n%d\n", outInt);
-	// printf("%s\n", ft_itoa(42));
+	//test for printing int values only (and result variable)
+	// int outInt =  ft_printf("%d%d", 42, 24);
+	// printf("\n%d\n", outInt);
+
+	// test for printing strings in combination with arguments (%d, %i, %s, %c, %%) and comparing it with og printf
+	int out = ft_printf("Hello, my name is %s and I am %d years old. :)\n%%\n%c%i\n", "Denice", 25, 'E', 1);
+	int out1 = printf("Hello, my name is %s and I am %d years old. :)\n%%\n%c%i\n", "Denice", 25, 'E', 1);
+	ft_printf("%d | %d\n", out, out1);
+
 	return 0;
 }
