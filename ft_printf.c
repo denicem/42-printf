224ef0f7191811f25d5017425fe06e1cb5fdf031
Denice Montemayor <dmontema@2-E-5.42heilbronn.de>
2021-10-31 17:54:01 +0100

    printing without any arguments now possible.

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: dmontema <dmontema@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/27 02:35:34 by dmontema          #+#    #+#             */
-/*   Updated: 2021/10/31 17:23:09 by dmontema         ###   ########.fr       */
+/*   Updated: 2021/10/31 17:50:55 by dmontema         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,13 +36,20 @@ int	ft_printf(const char *format, ...)
 	int args;
 
 	args = ft_countArgs(format);
+	if (!args)
+	{
+		args = ft_strlen(format);
+		ft_putstr_fd((char *) format, 1);
+	}
 
 	return (args);
 }
 
 int main() {
 	// printf("%d\n", ft_printf("%c%s%p%d%i%u%x%X%%"));
-	printf("%d\n", ft_printf("Hello, %s!\n"));
-	// printf("%%\n");
+	// printf("%d\n", ft_printf("Hello, my name is %s and I am %d years old. :)\n"));
+	int out = ft_printf("Hello, World!\n");
+	int out1 = ft_printf("H");
+	printf("\n%d %d\n", out, out1);
 	return 0;
 }
