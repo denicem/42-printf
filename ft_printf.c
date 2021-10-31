aecb6e80c87e8ab9ca799ea2ec93c9afa17be45e
Denice Montemayor <dmontema@2-E-5.42heilbronn.de>
2021-10-31 20:08:39 +0100

    implement prototype to print Integers.

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: dmontema <dmontema@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/27 02:35:34 by dmontema          #+#    #+#             */
-/*   Updated: 2021/10/31 17:50:55 by dmontema         ###   ########.fr       */
+/*   Updated: 2021/10/31 20:04:46 by dmontema         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,23 +33,42 @@ int	ft_countArgs(const char *format)
 
 int	ft_printf(const char *format, ...)
 {
-	int args;
+	int count_args;
+	va_list args;
 
-	args = ft_countArgs(format);
-	if (!args)
+	count_args = ft_countArgs(format);
+	if (!count_args)
 	{
-		args = ft_strlen(format);
+		count_args = ft_strlen(format);
 		ft_putstr_fd((char *) format, 1);
 	}
+	else
+	{
+		va_start(args, format);
+		int i = 0;
+		while (i < count_args)
+		{
+			int val = va_arg(args, int);
+			char *s = ft_itoa(val);
+			ft_putstr_fd(s, 1);
+			i++;
+		}
+	}
 
-	return (args);
+	return (count_args);
 }
 
 int main() {
 	// printf("%d\n", ft_printf("%c%s%p%d%i%u%x%X%%"));
 	// printf("%d\n", ft_printf("Hello, my name is %s and I am %d years old. :)\n"));
-	int out = ft_printf("Hello, World!\n");
-	int out1 = ft_printf("H");
-	printf("\n%d %d\n", out, out1);
+	// int out = ft_printf("Hello, World!\n");
+	// int out1 = ft_printf("H");
+	// int out = printf("Hello, World!\n");
+	// int out1 = printf("%d", 234);
+	// printf("\n%d %d\n", out, out1);
+
+	int outInt =  ft_printf("%d%d", 42, 24);
+	printf("\n%d\n", outInt);
+	// printf("%s\n", ft_itoa(42));
 	return 0;
 }
