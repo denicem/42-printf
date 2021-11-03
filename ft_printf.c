7a1b55c49c9caa602b64c72519b159ffa6817c6f
Denice Montemayor <dmontema@2-E-5.42heilbronn.de>
2021-11-03 22:11:15 +0100

    prototype for printing pointer address done.

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: dmontema <dmontema@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/27 02:35:34 by dmontema          #+#    #+#             */
-/*   Updated: 2021/11/03 21:15:25 by dmontema         ###   ########.fr       */
+/*   Updated: 2021/11/03 22:08:45 by dmontema         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,22 +15,6 @@
 #include "ft_printf.h"
 
 int	ft_putnbr_base(unsigned long nbr, char *set, unsigned base);
-
-// void	ft_putunbr_fd(int n, int fd)
-// {
-// 	if (n < 0)
-// 	{
-// 		n = INT_MAX + n;
-// 	}
-// 	// if (n < 0)
-// 	// {
-// 	// 	n = -n;
-// 	// 	ft_putchar_fd('-', fd);
-// 	// }
-// 	if (n > 9)
-// 		ft_putunbr_fd(n / 10, fd);
-// 	ft_putchar_fd(n % 10 + 48, fd);
-// }
 
 int	ft_countArgs(const char *format)
 {
@@ -129,11 +113,9 @@ int	ft_printf(const char *format, ...)
 				}
 				else if (*format == 'p')
 				{
-					long long unsigned lluVal = (long long unsigned) va_arg(args, void *);
-					int out = printf("0x%llx", lluVal);
-					// char *s = ft_itoa(intVal);
-					// ft_putstr_fd(s, 1);
-					count_args += out;
+					long unsigned luVal = (long unsigned) va_arg(args, void *);
+					ft_putstr_fd("0x", 1);
+					count_args += 1 + ft_putnbr_base(luVal, "0123456789abcdef", 16);
 					format++;
 					// count_args += (int) ft_strlen(s) - 1;
 				}
@@ -207,25 +189,10 @@ int main() {
 	// ft_printf("%d | %d\n", out, out1);
 
 	//test for printing pointer address
-	// int x = 10;
-	// int out1 = ft_printf("%p\n", &x);
-	// printf("\n");
-	// int out2 = printf("%p\n", &x);
-	// ft_printf("%d | %d", out1, out2);
-
-	// printf("%u\n", INT_MIN);
-	// printf("%u\n", INT_MAX);
-	// printf("%u\n", INT_MIN +1);
-	// printf("%u\n", -1);
-	// printf("%u\n", -2);
-	// printf("%u\n", -3);
-	// printf("%u\n", -4);
-	// printf("%u\n", -5);
-	// printf("%u\n", -6);
-	// int i = ft_putnbr_base(32, "01", 2);
-	// printf ("%d\n", i);
-
-	ft_printf("%u\n", -2);
+	int x = 10;
+	int out1 = ft_printf("%p\n", &x);
+	int out2 = printf("%p\n", &x);
+	ft_printf("%d | %d", out1, out2);
 
 	return 0;
 }
