8c993adf75dbcee1e5b2543f4f287ec6962f5b48
Denice Montemayor <dmontema@2-E-5.42heilbronn.de>
2021-11-04 21:15:10 +0100

    everything is structured; add one task.

--- ft_printf.c
+++ ft_printf.c
@@ -5,164 +5,109 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: dmontema <dmontema@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/10/27 02:35:34 by dmontema          #+#    #+#             */
-/*   Updated: 2021/11/03 22:08:45 by dmontema         ###   ########.fr       */
+/*   Created: 2021/11/04 17:07:33 by dmontema          #+#    #+#             */
+/*   Updated: 2021/11/04 21:10:59 by dmontema         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <limits.h>
 #include "ft_printf.h"
+#include <stdio.h>
 
-int	ft_putnbr_base(unsigned long nbr, char *set, unsigned base);
-
-int	ft_countArgs(const char *format)
+int ft_putnbr_int(int nbr)
 {
-	int res;
+	int	res;
 
 	res = 0;
-	while (*format)
+	if (nbr == INT_MIN)
 	{
-		if (*format == '%')
-		{
-			if (*(format + 1) == '%')
-				format++;
-			res++;
-		}
-		format++;
+		res += write(1, "-2", 2);
+		nbr = 147483648;
+	}
+	if (nbr < 0)
+	{
+		res += write(1, "-", 1);
+		nbr = -nbr;
+	}
+	if (nbr > 9)
+		res += ft_putnbr_int(nbr / 10);
+	res += ft_putchar(nbr % 10 + 48);
+	return (res);
+}
+
+int	ft_putnbr_base(unsigned long nbr, char *set, unsigned base)
+{
+	int	res;
+
+	res = 0;
+	if (nbr >= base)
+		res += ft_putnbr_base(nbr / base, set, base);
+	res += write(1, set + (nbr % base), 1);
+	return (res);
+}
+
+int	ft_putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+int ft_putstr(char *str)
+{
+	int	res;
+
+	if (str == NULL)
+		return (0);
+	res = 0;
+	while (*str)
+		res += ft_putchar(*str++);
+	return (res);
+}
+
+// TODO: from "else if: %u, ..." line too long, assign va_arg in one ul var and then call the funcs
+int	selectConvSpec(char **format, va_list *args)
+{
+	int	res;
+
+	res = 0;
+	if (**format == '%')
+		res+= (ft_putchar('%'));
+	else if (**format == 'c')
+		res += (ft_putchar(va_arg(*args, int)));
+	else if (**format == 's')
+		res += (ft_putstr(va_arg(*args, char *)));
+	else if (**format == 'd' || **format == 'i')
+		res += (ft_putnbr_int(va_arg(*args, int)));
+	else if (**format == 'u')
+		res += (ft_putnbr_base(va_arg(*args, unsigned long), "0123456789", 10));
+	else if (**format == 'x')
+		res += (ft_putnbr_base(va_arg(*args, unsigned long), "0123456789abcdef", 16));
+	else if (**format == 'X')
+		res += (ft_putnbr_base(va_arg(*args, unsigned long), "0123456789ABCDEF", 16));
+	else if (**format == 'p')
+	{
+		res += ft_putstr("0x");
+		res += (ft_putnbr_base(va_arg(*args, unsigned long), "0123456789abcdef", 16));
 	}
 	return (res);
 }
 
 int	ft_printf(const char *format, ...)
 {
-	int count_args;
-	va_list args;
-
-	count_args = ft_countArgs(format);
-	if (!count_args)
-	// PROTOTYPE print strings and chars, if there are no arguments
-	{
-		count_args = ft_strlen(format);
-		ft_putstr_fd((char *) format, 1);
-	}
-	// END PROTOTYPE print strings and chars, if there are no arguments
-	else
-	{
-		va_start(args, format);
-
-		// PROTOTYPE: printing arguments (i, d, s, c %%) with characters within
-		//TODO: add a func to select to select a specifier conversion. 
-		while (*format)
-		{
-			if (*format == '%')
-			{
-				format++;
-				if ((*format == 'd') || (*format == 'i'))
-				{
-					int intVal = va_arg(args, int);
-					char *s = ft_itoa(intVal);
-					ft_putstr_fd(s, 1);
-					format++;
-					count_args += (int) ft_strlen(s) - 1;
-				}
-				//usage of ft_putnbr_base works with every base including unsinged integer
-				else if (*format == 'x')
-				{
-					count_args += ft_putnbr_base(va_arg(args, long unsigned), "0123456789abcdef", 16); 
-					format++;
-				}
-				else if (*format == 'X')
-				{
-					count_args += ft_putnbr_base(va_arg(args, long unsigned), "0123456789ABCDEF", 16);
-					format++;
-				}
-				else if (*format == 'o')
-				{
-					count_args += ft_putnbr_base(va_arg(args, long unsigned), "01234567", 8);
-					format++;
-				}
-				else if (*format == 'b')
-				{
-					count_args += ft_putnbr_base(va_arg(args, long unsigned), "01", 2);
-					format++;
-				}
-				else if (*format == 'u')
-				{
-					count_args += ft_putnbr_base(va_arg(args, long unsigned), "0123456789", 10);
-					format++;
-				}
-				// END usage of ft_putnbr_base works with every base including unsinged integer
-				else if (*format == 's')
-				{
-					char *str = va_arg(args, char *);
-					ft_putstr_fd(str, 1);
-					format++;
-					count_args += (int) ft_strlen(str) - 1;
-				}
-				else if (*format == 'c')
-				{
-					char c = va_arg(args, int);
-					ft_putchar_fd(c, 1);
-					format++;
-				}
-				else if (*format == '%')
-				{
-					ft_putchar_fd('%', 1),
-					format++;
-				}
-				else if (*format == 'p')
-				{
-					long unsigned luVal = (long unsigned) va_arg(args, void *);
-					ft_putstr_fd("0x", 1);
-					count_args += 1 + ft_putnbr_base(luVal, "0123456789abcdef", 16);
-					format++;
-					// count_args += (int) ft_strlen(s) - 1;
-				}
-				else
-					format++;
-			}
-			else
-			{
-				ft_putchar_fd(*format, 1);
-				count_args++;
-				format++;
-			}
-		}
-		// END PROTOTYPE: printing arguments (i, d, s, c %%) with characters within
-
-		// PROTOTYPE: printing int values
-		// int i = 0;
-		// while (i < count_args)
-		// {
-		// 	int val = va_arg(args, int);
-		// 	char *s = ft_itoa(val);
-		// 	ft_putstr_fd(s, 1);
-		// 	i++;
-		// }
-		// END PROTOTYPE: printing int values
-
-	}
-
-	return (count_args);
-}
-
-///////////////////////////////////////////////////////////////////////////////
-
-// PROTOTYPE printing hexadecimal
-// void	put_nbrx(long long unsigned nbr) {}
-
-// PROTOTYPE printing unsigned integer
-int	ft_putnbr_base(unsigned long nbr, char *set, unsigned base)
-{
-	unsigned tmp;
-	int res;
+	int		res;
+	va_list	args;
 
 	res = 0;
-	if (nbr >= base)
-		res += ft_putnbr_base(nbr / base, set, base);
-	tmp = nbr % base;
-	res += write(1, set + tmp, 1);
+	va_start(args, format);
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			res += selectConvSpec((char **) &format, &args);
+		}
+		else
+			res += ft_putchar(*format);
+		format++;
+	}
 	return (res);
 }
 
@@ -193,6 +138,14 @@ int main() {
 	int out1 = ft_printf("%p\n", &x);
 	int out2 = printf("%p\n", &x);
 	ft_printf("%d | %d", out1, out2);
+
+	// int out = ft_printf("Hallo, %s.\nHappy Halloween%c\n", "Svenja", '!');
+	// int out1 = ft_printf("%d%i %X %x %u\n%d%%\n", 42, 24, 674, 1234, -23445, 100);
+	// ft_printf("%d, %d\n", out, out1);
+	// out = ft_printf("Hallo, %s.\nHappy Halloween%c\n", "Svenja", '!');
+	// out1 = ft_printf("%d%i %X %x %u\n%d%%\n", 42, 24, 674, 1234, -23445, 100);
+	// ft_printf("%d, %d\n", out, out1);
+
 
 	return 0;
 }
--- ft_printf.h
+++ ft_printf.h
@@ -6,11 +6,17 @@
 /*   By: dmontema <dmontema@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/27 02:35:30 by dmontema          #+#    #+#             */
-/*   Updated: 2021/10/31 17:32:44 by dmontema         ###   ########.fr       */
+/*   Updated: 2021/11/04 20:47:47 by dmontema         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include	<stdarg.h>
+# include	<limits.h>
 # include	"libft/libft.h"
 
 int ft_printf(const char *, ...);
+int ft_putnbr_int(int nbr);
+int	ft_putnbr_base(unsigned long nbr, char *set, unsigned base);
+int	ft_putchar(char c);
+int ft_putstr(char *str);
+int	selectConvSpec(char **format, va_list *args);
